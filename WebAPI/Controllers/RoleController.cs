05cf4d5ea8d680c8c20ffa0f7ac9ff140011b7d2
Arpit Bansal <arpit.bansal@conduent.com>
2019-06-04 17:35:51 +0530

    Remove Unused dlls reference

--- WebAPI/Controllers/RoleController.cs
+++ WebAPI/Controllers/RoleController.cs
@@ -1,10 +1,6 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
 using System.Web.Http;
 using System.Web.Http.Description;
 using WebAPI.Entity;
