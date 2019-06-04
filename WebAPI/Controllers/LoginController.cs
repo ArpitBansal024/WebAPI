63923adc2063932b937bda7444ddee7382b26288
Arpit Bansal <arpit.bansal@conduent.com>
2019-06-04 19:52:45 +0530

    Fixed Login controller CheckUser Method

--- WebAPI/Controllers/LoginController.cs
+++ WebAPI/Controllers/LoginController.cs
@@ -1,4 +1,10 @@
-﻿using System.Linq;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Web.Http;
 using System.Web.Http.Description;
 using WebAPI.Entity;
@@ -10,21 +16,44 @@ namespace WebAPI.Controllers
     {
         [HttpGet]
         [ResponseType(typeof(VMLogin))]
-        public IHttpActionResult CheckUser(string user, string pwd)
+        public HttpResponseMessage CheckUser(string user, string pwd)
         {
             using (DBEntities dBEntities = new DBEntities())
             {
                 var data = dBEntities.Logins.Where(s => s.Name == user && s.Password == pwd).
                     Select(w => new VMLogin
                     {
-                        id = w.id,
                         Name = w.Name,
                         Password = w.Password,
                         IsActive = w.IsActive,
-                        Message = w.IsActive == true ? "Success" : "User Account is DeActive"
-                    }).Take(1);//.FirstOrDefault();
+                        Message = w.IsActive == true ? "Success" : "User Account is De-Active"
+                    }).SingleOrDefault();//.FirstOrDefault();
+                return new HttpResponseMessage()
+                {
+                    Content = new StringContent(JObject.FromObject(data == null ? new object() : data).ToString(), Encoding.UTF8, "application/json")
+                };
+                // return Ok(data);
+            }
+        }
 
-                return Ok(data);
+
+        // Return Single Record Without Using HttpResponseMessage As Return Type
+        [HttpGet]
+        [ResponseType(typeof(VMLogin))]
+        public VMLogin CheckUserNew(string user, string pwd)
+        {
+            using (DBEntities dBEntities = new DBEntities())
+            {
+                var data = dBEntities.Logins.Where(s => s.Name == user && s.Password == pwd).
+                    Select(w => new VMLogin
+                    {
+                        Name = w.Name,
+                        Password = w.Password,
+                        IsActive = w.IsActive,
+                        Message = w.IsActive == true ? "Success" : "User Account is De-Active"
+                    }).SingleOrDefault();//.FirstOrDefault();
+                 return data;
+                // return Ok(data);
             }
         }
     }
